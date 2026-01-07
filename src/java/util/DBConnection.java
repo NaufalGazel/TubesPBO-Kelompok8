ef034f9b6c05b445f689bc933380fb999728cca5
rafifaiz12 <rafifaiz60@gmail.com>
2026-01-07 23:35:10 +0700

    edit database user from 'root' to 'app_user'

--- src/java/util/DBConnection.java
+++ src/java/util/DBConnection.java
@@ -8,7 +8,7 @@ public class DBConnection {
 
     private static final String URL =
         "jdbc:mysql://localhost:3306/hmit_db?useSSL=false&serverTimezone=UTC";
-    private static final String USER = "root";
+    private static final String USER = "app_user";
 
     private static final String PASSWORD =
         System.getenv("DB_PASSWORD");
